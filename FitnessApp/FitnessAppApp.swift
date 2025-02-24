e477e80b2839eb84ede15187b4c561b5a2199b0d
Debashish Boruah <debashishboruah54@gmail.com>
2025-02-24 13:08:39 +0530

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Debashish Boruah on 24/02/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
